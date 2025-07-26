95261dd02488059e22f152e15e5cccd52033a71a
FlashConCards <flashconcards@gmail.com>
2025-07-25 22:45:01 -0300

    Fix: adicionar configuração real do Firebase

--- app/lib/firebase-config.ts
+++ app/lib/firebase-config.ts
@@ -1,9 +1,10 @@
 // Configuração do Firebase para FlashConCards
 export const firebaseConfig = {
-  apiKey: "AIzaSyC_placeholder_key_here", // Você precisa substituir pela sua API key real
+  apiKey: "AIzaSyCNG3ozp8EoHSnR31-KBDI6bW0poRtUtQE",
   authDomain: "flashconcards-12244.firebaseapp.com",
   projectId: "flashconcards-12244",
-  storageBucket: "flashconcards-12244.appspot.com",
-  messagingSenderId: "105090638777285623074",
-  appId: "1:105090638777285623074:web:placeholder_app_id" // Você precisa substituir pelo seu App ID real
+  storageBucket: "flashconcards-12244.firebasestorage.app",
+  messagingSenderId: "671233635",
+  appId: "1:671233635:web:c3c44f8c379b674eedaa56",
+  measurementId: "G-HNHPF03X4K"
 }; 
